4609272cf3cc1a4fd1a8c428a7be63e600bc07fd
abcoathup <andrew@coathup.net>
2025-02-21 17:00:10 +1100

    add calendar

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,4 +15,20 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const events = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/events', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		startDate: z.coerce.date(),
+		endDate: z.coerce.date().optional(),
+		location: z.string().optional(),
+		heroImage: z.string().optional(),
+		url: z.string(),
+	}),
+});
+
+export const collections = { blog, events };
